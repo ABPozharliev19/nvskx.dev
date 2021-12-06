9516e9e02431320edc2b4b0db6db4d7131f97a86
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-12-06 17:59:41 +0200

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: "es5",
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    tabWidth: 4,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    jsxBracketSameLine: false,
+};
---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <div> Qsha! </div>
+  );
+}
+
+export default App;
